793c52841975e7850d6cb232bb836e133b106e08
Veltouille <julienstatho@orange.fr>
2018-10-16 22:20:53 +0200

    Standart Bullet
    Adding : model of standart Bullet
    
    New script : RedBullet - Behavior of the bullet
    
    Modifying TestMovement permit to open fire

---
+++ Assets/Scripts/RedBullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBullet : MonoBehaviour {
+
+
+    private string type = "red";
+
+    int _damage = 10;
+    int damage
+    {
+        get
+        {
+            return _damage;
+        }
+    }
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ApplyForce();
+
+        transform.parent = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (type == "red")
+        {
+            Debug.Log("Touch Aso");
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void ApplyForce()
+    {
+        Vector3 vector = Vector3.forward;
+        rb.AddForce(vector * 800);
+    }
+}
--- Assets/Scripts/TestMouvement.cs
+++ Assets/Scripts/TestMouvement.cs
@@ -6,6 +6,9 @@ public class TestMouvement : MonoBehaviour {
 
     public float speed;
 
+    public GameObject bullet;
+    public Transform bulletSpawn;
+
     private Rigidbody rb;
 
     void Start()
@@ -21,5 +24,13 @@ public class TestMouvement : MonoBehaviour {
         Vector3 movement = new Vector3(moveH, 0, moveV);
 
         rb.AddForce(movement * speed);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, bulletSpawn);
+        }
     }
 }
