1e6f9bac23783c7442ab6bab0698b6745135c657
Veltouille <julienstatho@orange.fr>
2018-10-18 23:47:50 +0200

    Enemy and kill Enemy (Red)
    Adding :
    
    The enemy (RedType) prefab
    
    Script RedEnemy
    
    Modification RedBullet to deal damage to Red Enemy
    
    Adding forms prefabs from Natures Guardian.

---
+++ Assets/Scripts/RedEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedEnemy : MonoBehaviour {
+
+    public string type = "red";
+
+    private int hp = 30;
+
+    public void TakeDamage(int damage, RedBullet bullet)
+    {
+        if (bullet.type == type)
+        {
+            hp -= damage;
+        }
+
+        if (hp <= 0)
+        {
+            BeKilled();
+        }
+    }
+
+    private void BeKilled()
+    {
+        Destroy(GetComponent<Collider>());
+        Destroy(gameObject, 2f);
+    }
+
+}
