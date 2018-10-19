5465da4cc09b46f55cf9e916d9a430cc009146ae
Veltouille <julienstatho@orange.fr>
2018-10-20 01:43:31 +0200

    Timer
    Adding :
    
    New prefab Global GUI for common GUI element in the game.
    
    Including : the Black Line, the timer Label and the GameOver Label.
    
    New Script : TimerGUI permitting the global cooldown and diplaying GameOver at when timer is at zero

---
+++ Assets/Scripts/TimerGUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerGUI : MonoBehaviour {
+
+    public Text timerLabel;
+    public Text gameOver;
+    
+    private float time = 121f;
+
+    private void Start()
+    {
+        gameOver.enabled = false;
+    }
+
+    private void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time >= 0f)
+        {
+            PrintTime();
+        }
+ 
+        if (time <= 0f)
+        {
+            DisplayGameOver();
+        }
+    }
+
+    private void PrintTime()
+    {
+        float minutes = (time / 60) - 0.5f;
+        float seconds = (time % 60) - 0.5f;
+
+        timerLabel.text = "Timer " + string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+
+    private void DisplayGameOver()
+    {
+        gameOver.enabled = true;
+    }
+}
