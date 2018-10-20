02a68f896828c15eb2a38fc0683e19d0861106ac
Veltouille <julienstatho@orange.fr>
2018-10-20 22:00:18 +0200

    RedSpawns
    Adding :
    
    New GameObjecet RedSpanw to pop mobs
    New Script RedSpawner to pop mobs from the RedSpawn

---
+++ Assets/Scripts/RedSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedSpawner : MonoBehaviour {
+
+    public RedEnemy redEnemy;
+
+    private float timeToNextSpawn;
+
+    private void Start()
+    {
+        timeToNextSpawn = CalculTimeToNextSpawn();
+    }
+
+    private void Update()
+    {
+        timeToNextSpawn -= Time.deltaTime;
+
+        if (timeToNextSpawn <= 0)
+        {
+            SpawnMob();
+
+            timeToNextSpawn = CalculTimeToNextSpawn();
+        }
+    }
+
+    private void SpawnMob()
+    {
+        Instantiate(redEnemy, transform.position, Quaternion.identity);
+    }
+
+    private float CalculTimeToNextSpawn()
+    {
+        float nextTime = 0f;
+
+        nextTime = Random.value * 10;
+
+        return nextTime;
+    }
+}
