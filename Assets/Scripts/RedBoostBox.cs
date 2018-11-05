22fc96fecb6b4c7be8873c1e049843b00e16db1f
Myrilandel <jonathan.navarro.amu@gmail.com>
2018-11-05 21:13:59 +0100

    Animations and RedBox

---
+++ Assets/Scripts/RedBoostBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBoostBox : MonoBehaviour {
+
+	[SerializeField] private int AmmoMinAmount;
+	[SerializeField] private int AmmoMaxAmount;
+	[SerializeField] private ParticleSystem PS_OnBoxPickedUp;
+	
+	private int AmmoValue;
+	private void Start () 
+	{
+		AmmoValue = Random.Range(AmmoMinAmount, AmmoMaxAmount+1);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag =="Player")
+		{
+            Debug.Log("Hello ammo");
+		}
+	}
+}
