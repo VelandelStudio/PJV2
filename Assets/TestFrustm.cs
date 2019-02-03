c1f2679b9b941e2ea26ecd810f1780ce5c8bdf86
Myrilandel <jonathan.navarro.amu@gmail.com>
2019-02-03 18:54:06 +0100

    Blue enemy renderer

---
+++ Assets/TestFrustm.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFrustm : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + 0.25f * transform.forward;
+    }
+}
