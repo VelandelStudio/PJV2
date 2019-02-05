6839e92ce8b22a34168484b26fba09178d17b621
maelleF <maelle.freteault@gmail.com>
2019-02-05 19:38:51 +0100

    Update Explosion.cs
    Correction of script:
    push back : use transform.translate and vector.back, and not force (wrong use of force)
    test succesfull on unity : Objects seem to be pushed, with less HP. still need others tests

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -11,14 +11,14 @@ public class Explosion : MonoBehaviour
 
     void Start()
     {
-        SetEnemyPower();
+        //SetEnemyPower();
         myCollider = GetComponent<SphereCollider>();
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        ExplosionEnd(); //don't really know where to put this one ...
+        ExplosionEnd(); //As the explosion only exist for an instant, end seems to be fitting here 
     }
 
     public void OnTriggerEnter(Collider other)
@@ -45,17 +45,17 @@ public class Explosion : MonoBehaviour
         PushBack(other);
     }
 
+    //this function needs to be reworked, otherwise the effect of distance from explosion doesn't work
     private void SetEnemyPower()
     {
-        powerAttack = GameManagement.instance.Level * 5;
+        powerAttack = GameManagement.instance.Level * powerAttack;
     }
 
     //push the object away from the center of explosion, with a higher force if object is close to it.
     private void PushBack(Collider other)
     {
-        Rigidbody otherRb = other.GetComponentInParent<Rigidbody>();
         Transform otherTransform = other.GetComponentInParent<Transform>();
-        otherRb.AddForce(-otherTransform.forward * ProximityOfCenter(other));
+        otherTransform.Translate(Vector3.back * ProximityOfCenter(other));
     }
 
     //closer the object is from the center, higher the impact of explosion will be.
@@ -65,6 +65,9 @@ public class Explosion : MonoBehaviour
         Transform otherPos = other.GetComponentInParent<Transform>();
         float distance = Vector3.Distance(transform.position, otherPos.position);
 
+        //depending whether the object is in the circle, distance can be negative or positive.
+        //to correctly measure the proximity, we need to take this in account
+        //example: for radius = 5, and distance from the center -4, the proximity is 1 (5 +(-4)) and not 9(5-(-4))
         if (distance < 0)
         {
             proximity = myCollider.radius + distance;
@@ -82,7 +85,7 @@ public class Explosion : MonoBehaviour
 
     private void ExplosionEnd()
     {
-        rb.isKinematic = true;
+        rb.isKinematic = true; //not sure if a rb fits an explosion tough ...
 
         Destroy(GetComponent<Collider>());
         Destroy(gameObject, 5f);
