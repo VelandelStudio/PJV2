9bcf7412b6581a99408876de5da97afd8849e5b5
Titoo-Wan <leglatintitouan@gmail.com>
2019-02-13 15:41:14 +0100

    Blue Enemy Movement + Detection

---
+++ Assets/Scripts/BlueEnemyMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// EnemyMovement class is used to make ennemies move and catch the player.
+/// </summary>
+public class EnemyMovement : MonoBehaviour {
+
+    Transform player;
+    NavMeshAgent nav;
+    EnemyAttack enemyAttack;
+
+    /// <summary>
+    /// On Awake, we get the ennemy components such as NavMeshAgent and EnnemyAttack.
+    /// We also get an instance of the player.
+    /// </summary>
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+        enemyAttack = GetComponent<EnemyAttack>();
+    }
+
+    /// <summary>
+    /// On Update, first we ceck if the navMeshAgent is not null.
+    /// If the ennemy is not attacking, he moves.
+    /// </summary>
+    private void Update()
+    {
+        if (nav)
+        {
+            if (enemyAttack.isAttacking)
+            {
+                nav.isStopped = true;
+            }
+            else
+            {
+                MoveToLocation(player.position);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Move the enemy to the target location.
+    /// </summary>
+    /// <param name="targetPoint"></param>
+    public void MoveToLocation(Vector3 targetPoint)
+    {
+        nav.destination = targetPoint;
+        nav.isStopped = false;
+    }
+}
