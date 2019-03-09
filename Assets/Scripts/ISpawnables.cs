4f2891e7c4c9aa2aed4ebd9fb10fab9c121db736
H-Soumia <46232195+H-Soumia@users.noreply.github.com>
2019-03-09 16:01:39 +0100

    Add files via upload

---
+++ Assets/Scripts/ISpawnables.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISpawnables
+{
+    int maxSpawn { get; }
+    int calculTimeToNextSpawn { get; }
+    void spawnMob();
+}
