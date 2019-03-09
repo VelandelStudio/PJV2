f2c01a703cb0dedc322fef246ce47f636c7e04c1
Myrilandel <jonathan.navarro.amu@gmail.com>
2019-03-09 12:42:17 +0100

    Repackaging Enemies APIs

---
+++ Assets/Scripts/LivingEntitiesAPI/ILivingEntities.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILivingEntities {
+    
+    int HP { get; }
+    void SetHP(int HP);
+    void Takedamage(int HP);
+    void Die();
+}
