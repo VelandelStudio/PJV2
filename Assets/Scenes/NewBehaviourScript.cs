a83effa6b03ec97ebac39e45a23af96448895203
NicolasSequeira <nicolassequeira@hotmail.fr>
2019-03-09 12:42:57 +0100

    Create NewBehaviourScript.cs

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemies{
+ 
+ int DetectionField{get;}
+ int score{get[]}
+ [Attack] attacks;
+ [Movement] movements;
+
+}
