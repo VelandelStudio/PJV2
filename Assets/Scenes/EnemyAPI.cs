c8cf15446c85799e97b70e5db45741550ee0de4b
NicolasSequeira <nicolassequeira@hotmail.fr>
2019-03-09 11:42:05 +0100

    IEnemy

---
+++ Assets/Scenes/EnemyAPI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILivingEntities {
+   void SetHP(int HP);
+   void Takedamage(int HP);
+   void Die();
+
+}
---
+++ Assets/Scripts/EnemyAPI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemes {
+   void setHP(int HP);
+   void takedamage(int HP);
+   void die();
+
+}
